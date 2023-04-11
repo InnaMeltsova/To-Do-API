8066c3d9f0699951aa0f6596995a36e0fcd18a66
 Inna Meltsova <innameltsova@Innas-Air.lan>
2023-04-11 14:49:07 -0400

    docker updates

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,18 @@ COPY . .
 
 EXPOSE 5000
 
+CMD ["npm", "run", "dev"]
+
+FROM node:18-alpine as production
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 5000
+
 CMD ["npm", "start"]
--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Delete, Put, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { CreateListDto, UpdateListDto } from './dto/lists.dto';
 
@@ -7,22 +7,26 @@ export class ListsController {
     constructor(private readonly listsService: ListsService) { }
 
     @Get()
+    @HttpCode(HttpStatus.OK)
     async getLists() {
-        await this.listsService.getLists();
+        return await this.listsService.getLists();
     }
 
     @Post('new')
+    @HttpCode(HttpStatus.CREATED)
     async createList(@Body() createListDTO: CreateListDto ) {
-        await this.listsService.createList(createListDTO);
+        return await this.listsService.createList(createListDTO);
     }
 
     @Put(':id')
+    @HttpCode(HttpStatus.OK)
     async updateList(@Param('id') id: string, @Body() updateListDto: UpdateListDto) {
-        await this.listsService.updateList(id, updateListDto);
+        return await this.listsService.updateList(id, updateListDto);
     }
 
     @Delete(':id')
+    @HttpCode(HttpStatus.OK)
     async deleteList(@Param('id') id: string) {
-        await this.listsService.deleteList(id);
+        return await this.listsService.deleteList(id);
     }
 }
